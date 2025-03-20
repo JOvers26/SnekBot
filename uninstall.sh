9180add89aab1c60cbf820e3cd8e7bbbba61d0be
Overkill26 <jover26@outlook.com>
2025-03-20 10:03:47 +1000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sudo apt remove ~nros-jazzy-* && sudo apt autoremove'
+sudo apt remove ~nros-jazzy-* && sudo apt autoremove
 sudo rm /etc/apt/sources.list.d/ros2.list
 sudo apt -y update
 sudo apt iy autoremove
-# Consider upgrading for packages previously shadowed.
 sudo apt -y upgrade
 cd $HOME
 rm -r SnekBot/
