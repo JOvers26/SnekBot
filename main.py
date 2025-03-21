50b67e3718fb378fa267896d5676518c7b1e5cab
Overkill26 <jover26@outlook.com>
2025-03-21 10:39:12 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from snekbot_publisher import SnekBotPublisher
 import rclpy
+import random 
 
 def main():
     rclpy.init()  # Initialize ROS 2
@@ -8,10 +9,12 @@ def main():
     publisher_node = SnekBotPublisher()
 
     # Send a position message
-    publisher_node.send_position("Position: (1, 2, 3)")
+    while True:
 
-    # Keep the node running and processing messages
-    publisher_node.spin()
+        publisher_node.send_position(str(random.randint(0, 100)))
+
+        # Keep the node running and processing messages
+        publisher_node.spin()
 
     # Shutdown ROS 2 once done
     rclpy.shutdown()
