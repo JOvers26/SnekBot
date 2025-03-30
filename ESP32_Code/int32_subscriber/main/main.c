ddf96610aa80b03a021af473e0442a2c78f85105
Jover26 <102941061+Jover26@users.noreply.github.com>
2025-03-30 15:34:52 +1000

    Update main.c

--- ESP32_Code/int32_subscriber/main/main.c
+++ ESP32_Code/int32_subscriber/main/main.c
@@ -11,7 +11,7 @@
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <std_msgs/msg/float64.h>
-#include <std_msgs/msg/float64_multi_array.h>
+#include <sensor_msgs/msg/joint_state.h>  // Use JointState message type
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
@@ -19,31 +19,38 @@
 #include <rmw_microros/rmw_microros.h>
 #endif
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);} }
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);} }
 
 rcl_subscription_t gripper_subscriber;
 rcl_subscription_t joint_states_subscriber;
 std_msgs__msg__Float64 recv_gripper_msg;
-std_msgs__msg__Float64MultiArray recv_joint_states_msg;
+sensor_msgs__msg__JointState recv_joint_states_msg;  // Update to JointState message type
 
+// Gripper subscription callback function
 void gripper_subscription_callback(const void * msgin)
 {
     const std_msgs__msg__Float64 * msg = (const std_msgs__msg__Float64 *)msgin;
     printf("Received gripper position: %.2f\n", msg->data);
 }
 
+// Joint states subscription callback function
 void joint_states_subscription_callback(const void * msgin)
 {
-    const std_msgs__msg__Float64MultiArray * msg = (const std_msgs__msg__Float64MultiArray *)msgin;
-    printf("Received joint states: %s\n", (char*)msg->data.data);
+    const sensor_msgs__msg__JointState * msg = (const sensor_msgs__msg__JointState *)msgin;
+    printf("Received joint states:\n");
+    for (int i = 0; i < msg->position.size; i++) {
+        printf("Joint %d position: %.2f\n", i, msg->position.data[i]);
+    }
 }
 
+// Micro-ROS task to handle subscriptions and execution loop
 void micro_ros_task(void * arg)
 {
     rcl_allocator_t allocator = rcl_get_default_allocator();
     rclc_support_t support;
 
+    // Initialize RCL options
     rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
     RCCHECK(rcl_init_options_init(&init_options, allocator));
 
@@ -52,33 +59,42 @@ void micro_ros_task(void * arg)
     RCCHECK(rmw_uros_options_set_udp_address(CONFIG_MICRO_ROS_AGENT_IP, CONFIG_MICRO_ROS_AGENT_PORT, rmw_options));
 #endif
 
+    // Initialize RCL support
     RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
 
+    // Create the ROS node
     rcl_node_t node = rcl_get_zero_initialized_node();
     RCCHECK(rclc_node_init_default(&node, "gripper_joint_states_subscriber_rclc", "", &support));
 
+    // Initialize subscription for gripper position
     RCCHECK(rclc_subscription_init_default(
         &gripper_subscriber,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64),
         "snekbot/gripper_position"));
 
+    // Initialize subscription for joint states
     RCCHECK(rclc_subscription_init_default(
         &joint_states_subscriber,
         &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64MultiArray),
+        ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState),  // Use JointState
         "snekbot/joint_states"));
 
+    // Create executor for the subscriptions
     rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
     RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+
+    // Add subscriptions to executor
     RCCHECK(rclc_executor_add_subscription(&executor, &gripper_subscriber, &recv_gripper_msg, &gripper_subscription_callback, ON_NEW_DATA));
     RCCHECK(rclc_executor_add_subscription(&executor, &joint_states_subscriber, &recv_joint_states_msg, &joint_states_subscription_callback, ON_NEW_DATA));
 
+    // Spin to handle callbacks
     while (1) {
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
         usleep(10000);
     }
 
+    // Clean up
     RCCHECK(rcl_subscription_fini(&gripper_subscriber, &node));
     RCCHECK(rcl_subscription_fini(&joint_states_subscriber, &node));
     RCCHECK(rcl_node_fini(&node));
@@ -88,10 +104,12 @@ void micro_ros_task(void * arg)
 
 void app_main(void)
 {
+    // Initialize network interface
 #if defined(CONFIG_MICRO_ROS_ESP_NETIF_WLAN) || defined(CONFIG_MICRO_ROS_ESP_NETIF_ENET)
     ESP_ERROR_CHECK(uros_network_interface_initialize());
 #endif
 
+    // Create the micro-ROS task
     xTaskCreate(micro_ros_task,
                 "uros_task",
                 CONFIG_MICRO_ROS_APP_STACK,
