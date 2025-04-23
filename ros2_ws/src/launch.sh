99c85120db1afcf4dde5aff8d070dcf3473414b9
Overkill26 <jover26@outlook.com>
2025-04-24 09:24:37 +1000

    Update launch.sh

--- ros2_ws/src/launch.sh
+++ ros2_ws/src/launch.sh
@@ -4,5 +4,6 @@ colcon build --symlink-install
 source install/setup.bash
 
 
-ros2 run foxglove_bridge foxglove_bridge
-ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888
+ros2 run foxglove_bridge foxglove_bridge &
+ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888 &
+wait
