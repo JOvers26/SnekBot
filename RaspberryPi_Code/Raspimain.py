bdee3856fcc2eb8a2255361a1f6f30bcd94f846c
Overkill26 <jover26@outlook.com>
2025-04-25 18:06:05 +1000

    Update Raspimain.py

--- RaspberryPi_Code/Raspimain.py
+++ RaspberryPi_Code/Raspimain.py
@@ -64,9 +64,9 @@ while running:
         pivot = 0
         hat = joystick.get_hat(0)
         if hat[1] == 1:
-            pivot = -0.06
+            pivot = -0.03
         elif hat[1] == -1:
-            pivot = 0.06
+            pivot = 0.03
 
         if pivot != 0:
             snekbot.increment_joint1(pivot)
